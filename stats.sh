9fd481f77ee9fb7461f263ad73d2feeccda1ab45
Daniel Stenberg <daniel@haxx.se>
2015-12-09 03:57:22 +0100

    stats: fix the words per section stat

--- stats.sh
+++ stats.sh
@@ -4,18 +4,19 @@ book=`grep -o '[a-z-]*\.md' SUMMARY.md`
 
 tbd=`cat $book | grep -c ^TBD`
 titles=`cat $book | grep -c ^#`
+content=`echo "$titles-$tbd" | bc`
 
 words=`cat $book | wc -w`
 lines=`cat $book | wc -l`
 
-echo "Titles added: $titles"
+echo "Section titles: $titles"
+echo "Sections with content: $content"
 echo "Sections to go: $tbd"
 echo "Words: $words"
 echo "Lines: $lines"
 
-complete=`echo "scale=1; ($titles-$tbd)*100/$titles" | bc`
-
-secsize=`echo "$words/$complete" | bc`
+complete=`echo "scale=1; $content*100/$titles" | bc`
+secsize=`echo "$words/$content" | bc`
 
 echo "Words per section: $secsize"
 echo "Completeness: $complete %"

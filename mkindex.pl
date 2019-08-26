1eac5dd7bb763665f2347035dba6c2a95ff25bc9
Daniel Stenberg <daniel@haxx.se>
2019-08-26 10:54:00 +0200

    mkindex: generate only one link per page
    If the word is found in multiple places within the same page, skip them
    in an attempt to unclutter the index somewhat. Only link the first
    occurance in a page.

--- mkindex.pl
+++ mkindex.pl
@@ -69,8 +69,8 @@ sub single {
             $w =~ s/[,\.\`\']//g;
             $w = folded($w);
             if($index{$w}) {
-                if(!$word{$w}{$url}) {
-                    $word{$w}{$url}++;
+                if(!$word{$w}{$fname}) {
+                    $word{$w}{$fname}++;
                     $all{$w} .= ($all{$w}?", ":"")."[$section]($url)";
                 }
             }
@@ -78,9 +78,9 @@ sub single {
         # check longer words
         foreach my $w (@lwords) {
             if(folded($l) =~ /$w/) {
-                if(!$word{$w}{$url}) {
+                if(!$word{$w}{$fname}) {
                     #print " $w ($url)\n";
-                    $word{$w}{$url}++;
+                    $word{$w}{$fname}++;
                     $all{$w} .= ($all{$w}?", ":"")."[$section]($url)";
                 }
             }

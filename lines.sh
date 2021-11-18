866ce82fbe82b3637be69e67ae273222dfc305b4
Daniel Stenberg <daniel@haxx.se>
2021-11-18 15:02:30 +0100

    lines: fixed

--- lines.sh
+++ lines.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-book=`grep -o '[a-z-]*\.md' SUMMARY.md`
+book=`grep -o '[/a-z-]*\.md' SUMMARY.md`
 
-wc -l README.md $book | grep -v " total" | sort -nr
+wc -l -- README.md $book | grep -v " total" | sort -nr

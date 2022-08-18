f123e57bf12de94eddb1724180a287ed16797e26
Daniel Stenberg <daniel@haxx.se>
2022-08-18 15:08:03 +0200

    stats: remove some of the "aim" data and make it pure stats

--- stats.sh
+++ stats.sh
@@ -9,19 +9,24 @@ content=`echo "$titles-$tbd" | bc`
 
 words=`cat $book | wc -w`
 lines=`cat $book | wc -l`
+nonspace=`cat $book | tr -d ' #' | wc -c`
 
 echo "Section titles: $titles"
-echo "Sections with content: $content"
-echo "Sections to go: $tbd"
+#echo "Sections with content: $content"
+#echo "Sections to go: $tbd"
 echo "Words: $words"
 echo "Lines: $lines"
+echo "Non-space bytes: $nonspace"
+
+wordlen=`echo "scale=2; $nonspace / $words" | bc`
+echo "Average word length: $wordlen characters"
 
 complete=`echo "scale=1; $content*100/$titles" | bc`
 secsize=`echo "$words/$content" | bc`
 
 echo "Words per section: $secsize"
-echo "Completeness: $complete %"
+#echo "Completeness: $complete %"
 
 left=`echo "$tbd * $secsize" | bc`
 total=`echo "$secsize * $titles" | bc`
-echo "Estimated $left words left until $total"
+#echo "Estimated $left words left until $total"

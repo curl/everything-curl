8eb1bf3a91861711035ca19da782d8103f6dfa96
sluicing <joe.k963+sluicing@gmail.com>
2023-12-27 18:58:44 -0800

    Update verify script to support explicit section ID links

--- .github/scripts/verify-titles.pl
+++ .github/scripts/verify-titles.pl
@@ -1,11 +1,17 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
 my $sum = shift @ARGV;
 
 # Figure out all files in right order
 open(F, "<$sum");
 while(<F>) {
-    if($_  =~ /\[(.*)\]\(([^\)]*)\)/) {
+    if ($_ =~ /\[(.*)\]\(([^\)\#]*)\#([^\)]+)\)/) {
+        my ($title, $file, $section_id) = ($1, $2, $3);
+        push @files, $file;
+        $title{$file} = $title;
+        $sections{$file} = $section_id;
+    }
+    elsif($_  =~ /\[(.*)\]\(([^\)]*)\)/) {
         my ($title, $file) = ($1, $2);
         push @files, $file;
         $title{$file} = $title;
@@ -13,15 +19,24 @@ while(<F>) {
 }
 close(F);
 
+my $errors = 0;
+
 sub check {
     my ($f) = @_;
-    open(F, "<$f");
+    open(F, "<$f") || die("ouch $f");
     while(<F>) {
         if(/^# (.*)/) {
-            # verify
-            if($1 ne $title{$f}) {
+            my $expected;
+            if(exists $sections{$f}) {
+                # This file was linked from SUMMARY.md with a section header
+                $expected = "$title{$f} {#$sections{$f}}";
+            } else {
+                # This file was linked with no section header
+                $expected = $title{$f};
+            }
+            if ($1 ne $expected) {
                 printf STDERR "$f says '%s', not '%s' like SUMMARY.md\n",
-                    $1, $title{$f};
+                    $1, $expected;
                 $errors++;
             }
             last;

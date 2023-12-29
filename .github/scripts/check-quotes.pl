ccc12e2c3574a8a453898f978eefc4bcbd684c69
Daniel Stenberg <daniel@haxx.se>
2023-12-29 16:12:56 +0100

    CI: check for "bad" quotes

---
+++ .github/scripts/check-quotes.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+
+my $sum = shift @ARGV;
+
+# Figure out all files in right order
+open(F, "<$sum");
+while(<F>) {
+    if ($_ =~ /\[(.*)\]\(([^\)\#]*)\#([^\)]+)\)/) {
+        my ($title, $file, $section_id) = ($1, $2, $3);
+        push @files, $file;
+    }
+    elsif($_  =~ /\[(.*)\]\(([^\)]*)\)/) {
+        my ($title, $file) = ($1, $2);
+        push @files, $file;
+    }
+}
+close(F);
+
+my $errors = 0;
+
+sub check {
+    my ($f) = @_;
+    open(F, "<$f") || die("ouch $f");
+    my $l = 1;
+    while(<F>) {
+        if(/^(~~~|```)/) {
+            print STDERR "$f:$l:1: uses $1, not 4-space indent\n";
+            $errors++;
+        }
+        $l++;
+    }
+    close(F);
+};
+
+for my $f (@files) {
+    check($f);
+}
+
+exit $errors;
